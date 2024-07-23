0e753d0c7fd9b97d369c184f98a84152f3357cd0
Yunnglin <maoyl@smail.nju.edu.cn>
2024-07-23 19:16:35 +0800

    update argument

--- vlmeval/utils/arguments.py
+++ vlmeval/utils/arguments.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Optional, List, Union
+from os import environ
+
 
 @dataclass
 class Arguments:
@@ -7,6 +9,7 @@ class Arguments:
     model: List[str] = field(default_factory=list)
     nframe: int = 8
     pack: bool = False
+    use_subtitle: bool = False
     work_dir: str = '.'
     mode: str = 'all'
     nproc: int = 1
@@ -16,6 +19,15 @@ class Arguments:
     ignore: bool = False
     rerun: bool = False
     limit: Optional[int] = None
+
+    # For OpenAI API
     OPENAI_API_KEY: Optional[str] = None
     OPENAI_API_BASE: Optional[str] = None
     LOCAL_LLM: Optional[str] = None
+
+    def __post_init__(self):
+        environ.update({
+            'OPENAI_API_KEY': self.OPENAI_API_KEY,
+            'OPENAI_API_BASE': self.OPENAI_API_BASE,
+            'LOCAL_LLM': self.LOCAL_LLM
+        })
